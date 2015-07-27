4903ff939c56f5009eeb58bb289109bd8a2e8ea6
niklasskoldmark <niklas.skoldmark@gmail.com>
2015-07-27 22:02:00 +0200

    Initial Release

---
+++ Dockerfile
@@ -0,0 +1,101 @@
+FROM debian:8.1
+
+MAINTAINER Niklas Skoldmark <niklas.skoldmark@gmail.com>
+
+RUN apt-get update && \
+    DEBIAN_FRONTEND=noninteractive \
+    apt-get install \
+    -y \
+        apache2 \
+        build-essential \
+        graphviz \
+        inotify-hookable \
+        libapache-session-perl \
+        libapache2-authcookie-perl \
+        libapache2-mod-perl2 \
+        libapache2-request-perl \
+        libapache2-sitecontrol-perl \
+        libauthen-radius-perl \
+        libbind-config-parser-perl \
+        libcarp-assert-perl \
+        libcgi-pm-perl \
+        libclass-dbi-abstractsearch-perl \
+        libclass-dbi-perl \
+        libcrypt-cast5-perl \
+        libdbd-mysql-perl \
+        libdigest-sha-perl \
+        libfile-spec-perl \
+        libgraphviz-perl \
+        libhtml-mason-perl \
+        liblog-dispatch-perl \
+        liblog-log4perl-perl \
+        libmodule-build-perl \
+        libnet-appliance-session-perl \
+        libnet-dns-perl \
+        libnet-dns-zonefile-fast-perl \
+        libnet-irr-perl \
+        libnet-patricia-perl \
+        libnetaddr-ip-perl \
+        libparallel-forkmanager-perl \
+        librrds-perl \
+        libsnmp-info-perl \
+        libsocket6-perl \
+        libsql-translator-perl \
+        libssl-dev \
+        libtest-simple-perl \
+        libtime-local-perl \
+        liburi-perl \
+        libxml-simple-perl \
+        postfix \
+        rrdtool \
+        snmp \
+        mysql-client \
+        curl \
+    && \
+    apt-get clean && \
+    rm -rf /tmp/* && \
+    rm -rf /var/tmp/* && \
+    rm -rf /var/lib/apt/lists/*
+    
+RUN cd /srv && \
+    curl -L "http://downloads.sourceforge.net/project/netdisco/netdisco-mibs/latest-snapshot/netdisco-mibs-snapshot.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fnetdisco%2Ffiles%2Fnetdisco-mibs%2Flatest-snapshot%2F&ts=1393793276&use_mirror=heanet" |tar zxvf - && \
+    mkdir -p /usr/local/netdisco/mibs/ && \
+    cp -R /srv/netdisco-mibs/* /usr/local/netdisco/mibs/ && \
+    echo 'deb http://ftp.se.debian.org/debian jessie main non-free' >> /etc/apt/sources.list && \
+    apt-get update && apt-get install -y snmp-mibs-downloader && \
+    sed -i -e "s/mibs :.*/#mibs :/g" /etc/snmp/snmp.conf && \
+    echo 'mibdirs +/usr/local/netdisco/mibs/' >> /usr/share/snmp/snmp.conf
+
+COPY ["oui.txt", "/srv/"]
+
+RUN cd /srv && \
+    curl -L "http://netdot.uoregon.edu/pub/dists/netdot-1.0.7.tar.gz" |tar zxvf - && \
+    cd netdot* && \
+    cp etc/Default.conf etc/Site.conf && \
+    sed -i -e "s/^SENDMAIL\s.*/SENDMAIL => \'\/usr\/sbin\/postfix\',/g" etc/Site.conf && \
+    sed -i -e "s/SNMP_MIBS_PATH.*/SNMP_MIBS_PATH  => \'\/usr\/local\/netdisco\/mibs\',/g" etc/Site.conf && \
+    sed -i -e "s/DEVICE_NAMING_METHOD_ORDER.*/DEVICE_NAMING_METHOD_ORDER  => [ \'sysname\', \'snmp_target\'],/g" etc/Site.conf && \
+    make install APACHEUSER=www-data APACHEGROUP=www-data && \
+    cp /usr/local/netdot/etc/netdot_apache24_local.conf /usr/local/netdot/etc/running_apache.conf && \
+    ln -s /usr/local/netdot/etc/running_apache.conf /etc/apache2/sites-available/netdot.conf && \
+    mv /srv/oui.txt bin/
+
+# Backup initial /usr/local/netdot/etc/
+RUN mkdir /usr/local/netdot/etcbck && \
+    cp -R /usr/local/netdot/etc/* /usr/local/netdot/etcbck/
+
+# Backup initial /usr/local/netdisco/mibs
+RUN mkdir /usr/local/netdisco/mibsbck && \
+    cp -R /usr/local/netdisco/mibs/* /usr/local/netdisco/mibsbck/
+
+# Backup initial /usr/local/netdot/export/cacti
+RUN mkdir /usr/local/netdot/export/cactibck && \
+    cp -R /usr/local/netdot/export/cacti/* /usr/local/netdot/export/cactibck/
+
+EXPOSE 80
+
+VOLUME /usr/local/netdot/etc /usr/local/netdot/export/bind /usr/local/netdot/export/cacti /usr/local/netdot/export/dhcpd /usr/local/netdot/export/docs /usr/local/netdot/export/ethers /usr/local/netdot/export/nagios /usr/local/netdot/export/rancid /usr/local/netdot/export/smokeping /usr/local/netdot/export/sysmon /usr/local/netdisco/mibs
+
+COPY ["entrypoint.sh", "/srv/entrypoint.sh"]
+
+ENTRYPOINT ["/srv/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,155 @@
+#!/bin/sh
+#################################################################
+# Set variables
+netdotpath="/usr/local/netdot"
+configfile="$netdotpath/etc/Site.conf"
+#configfile="/srv/netdot-1.0.7/etc/Site.conf"
+
+#################################################################
+# Setup postfix
+postconf -e relayhost="$POSTFIX_RELAY_PORT_25_TCP_ADDR"
+
+#################################################################
+# If /usr/local/netdot/etc/ is empty (mounted volume), copy the initial netdot configs
+[ ! "$(ls -A /usr/local/netdot/etc/ )" ] && \
+cp -R /usr/local/netdot/etcbck/* /usr/local/netdot/etc/
+
+#################################################################
+# If /usr/local/netdisco/mibs/ is empty (mounted volume), copy the initial mibs
+[ ! "$(ls -A /usr/local/netdisco/mibs/ )" ] && \
+cp -R /usr/local/netdisco/mibsbck/* /usr/local/netdisco/mibs/
+
+#################################################################
+# If /usr/local/netdot/export/cacti/ is empty (mounted volume), copy the initial cacti configs
+[ ! "$(ls -A /usr/local/netdot/export/cacti/ )" ] && \
+cp -R /usr/local/netdot/export/cactibck/* /usr/local/netdot/export/cacti/
+
+#################################################################
+# Fix access
+chmod -R 777 /usr/local/netdisco/mibs/ && \
+chmod -R 777 /usr/local/netdot/etc/ && \
+chmod -R 777 /usr/local/netdot/export
+
+#################################################################
+# Fix /usr/local/netdot/etc/Site.conf
+#################################################################
+# Database setup
+sed -i -e "s/^DB_TYPE\s.*/DB_TYPE => \'mysql\',/g" "$configfile"
+sed -i -e "s/^DB_HOME\s.*/DB_HOME => \'\/usr\',/g" "$configfile"
+sed -i -e "s/^DB_DBA\s.*/DB_DBA => \'root\',/g" "$configfile"
+sed -i -e "s/^DB_DBA_PASSWORD\s.*/DB_DBA_PASSWORD => \'$MYSQL_ENV_MYSQL_ROOT_PASSWORD\',/g" "$configfile"
+sed -i -e "s/^DB_HOST\s.*/DB_HOST => \'$MYSQL_PORT_3306_TCP_ADDR\',/g" "$configfile"
+sed -i -e "s/^DB_PORT\s.*/DB_PORT => \'$MYSQL_PORT_3306_TCP_PORT\',/g" "$configfile"
+# Set this to the canonical name of the interface NetDoT will be talking to the database on.
+# If you said that the DB_HOST above was "localhost," this should be too.
+# This value will be used to grant NetDoT access to the database.
+# If you want to access the NetDoT database from multiple hosts, you'll need to grant those database rights by hand.
+#DB_NETDOT_HOST =>  'localhost',
+sed -i -e "s/^DB_NETDOT_HOST\s.*/DB_NETDOT_HOST => \'\%\',/g" "$configfile"
+sed -i -e "s/^DB_DATABASE\s.*/DB_DATABASE => \'netdot\',/g" "$configfile"
+sed -i -e "s/^DB_NETDOT_USER\s.*/DB_NETDOT_USER => \'netdot_user\',/g" "$configfile"
+sed -i -e "s/^DB_NETDOT_PASS\s.*/DB_NETDOT_PASS => \'netdot_pass\',/g" "$configfile"
+#####################################################################
+# Contact stuff
+sed -i -e "s/^SENDMAIL\s.*/SENDMAIL => \'\/usr\/sbin\/postfix\',/g" "$configfile"
+#####################################################################
+# SNMP specific
+sed -i -e "s/^SNMP_MIBS_PATH\s.*/SNMP_MIBS_PATH => \'\/usr\/local\/netdisco\/mibs\',/g" "$configfile"
+#####################################################################
+# Device Management
+sed -i -e "s/^POLL_STATS_FILE_PATH\s.*/POLL_STATS_FILE_PATH => \'\/var\/pollstats.rrd\',/g" "$configfile"
+#####################################################################
+# Misc
+sed -i -e "s/^TMP\s.*/TMP => \"\/usr\/local\/netdot\/tmp\",/g" "$configfile"
+sed -i -e "s/^NETDOT_PATH\s.*/NETDOT_PATH => \"\/usr\/local\/netdot\",/g" "$configfile"
+#####################################################################
+#  - NAGIOS - www.nagios.org
+sed -i -e "s/^NAGIOS_DIR\s.*/NAGIOS_DIR => \'\/usr\/local\/netdot\/export\/nagios\',/g" "$configfile"
+#####################################################################
+#  - SYSMON - www.sysmon.org
+sed -i -e "s/^SYSMON_DIR\s.*/SYSMON_DIR => \'\/usr\/local\/netdot\/export\/sysmon\',/g" "$configfile"
+#####################################################################
+#  - RANCID - http://www.shrubbery.net/rancid/
+sed -i -e "s/^RANCID_DIR\s.*/RANCID_DIR => \'\/usr\/local\/netdot\/export\/rancid\',/g" "$configfile"
+#####################################################################
+#  - BIND - www.isc.org
+sed -i -e "s/^BIND_EXPORT_DIR\s.*/BIND_EXPORT_DIR => \'\/usr\/local\/netdot\/export\/bind\',/g" "$configfile"
+#####################################################################
+#  - DHCPD - www.isc.org
+sed -i -e "s/^DHCPD_EXPORT_DIR\s.*/DHCPD_EXPORT_DIR => \'\/usr\/local\/netdot\/export\/dhcpd\',/g" "$configfile"
+#####################################################################
+#  - Smokeping - http://oss.oetiker.ch/smokeping/
+sed -i -e "s/^SMOKEPING_DIR\s.*/SMOKEPING_DIR => \'\/usr\/local\/netdot\/export\/smokeping\',/g" "$configfile"
+
+#################################################################
+# Wait for MYSQL server availability
+while ! exec 6<>/dev/tcp/$MYSQL_PORT_3306_TCP_ADDR/$MYSQL_PORT_3306_TCP_PORT 
+do
+  echo "$(date) - still trying"
+  sleep 1
+done
+
+#################################################################
+# Install database if not done
+configfile="etc/Site.conf"
+
+if [ ! -f /srv/netdotdbinitdone ];
+then
+    cd /srv/netdot* && \
+    sed -i -e "s/^DB_TYPE\s.*/DB_TYPE => \'mysql\',/g"  "$configfile" && \
+    sed -i -e "s/^DB_HOME\s.*/DB_HOME => \'\/usr\',/g"  "$configfile" && \
+    sed -i -e "s/^DB_DBA\s.*/DB_DBA => \'root\',/g"  "$configfile" && \
+    sed -i -e "s/^DB_DBA_PASSWORD\s.*/DB_DBA_PASSWORD => \'$MYSQL_ENV_MYSQL_ROOT_PASSWORD\',/g"  "$configfile" && \
+    sed -i -e "s/^DB_HOST\s.*/DB_HOST => \'$MYSQL_PORT_3306_TCP_ADDR\',/g"  "$configfile" && \
+    sed -i -e "s/^DB_PORT\s.*/DB_PORT => \'$MYSQL_PORT_3306_TCP_PORT\',/g"  "$configfile" && \
+    sed -i -e "s/^DB_NETDOT_HOST\s.*/DB_NETDOT_HOST => \'\%\',/g"  "$configfile" && \
+    sed -i -e "s/^DB_DATABASE\s.*/DB_DATABASE => \'netdot\',/g"  "$configfile" && \
+    sed -i -e "s/^DB_NETDOT_USER\s.*/DB_NETDOT_USER => \'netdot_user\',/g"  "$configfile" && \
+    sed -i -e "s/^DB_NETDOT_PASS\s.*/DB_NETDOT_PASS => \'netdot_pass\',/g"  "$configfile" && \
+    if [ ! -f bin/oui.txt ] ;
+    then
+        sed -i "/.*oui.txt.*/s/^/#/" bin/Makefile && \
+        make installdb && \
+        sed -i "/^#.*oui.txt.*/s/^#//" bin/Makefile
+    else
+    	make installdb
+    fi  && \
+    echo "GRANT ALL ON netdot.* TO netdot_user@'%' IDENTIFIED BY 'netdot_pass' WITH GRANT OPTION; FLUSH PRIVILEGES" | \
+    mysql -uroot -p$MYSQL_ENV_MYSQL_ROOT_PASSWORD -h$MYSQL_PORT_3306_TCP_ADDR && \
+    touch /srv/netdotdbinitdone
+fi
+
+#################################################################
+# apache2
+# If site is enabled, disable it (apache seems to crash otherwise)
+a2dissite netdot.conf
+# Start apache2
+service apache2 start
+# Enable the netdot.conf
+a2ensite netdot.conf
+# Reload apache2
+service apache2 reload
+
+#################################################################
+# Watch directories for changes, reload apache2 and fix permissions if changed
+while : ; do
+    inotify-hookable \
+        --watch-directories /usr/local/netdisco/mibs/ \
+        --watch-directories /usr/local/netdot/etc/ \
+        --watch-directories /usr/local/netdot/export/ \
+        --on-modify-path-command "( \
+        	    /usr/local/netdisco/mibs/ \
+        	| \
+        	    /usr/local/netdot/etc/ \
+            )=( \
+                service apache2 reload && \
+                chmod -R 777 /usr/local/netdisco/mibs/ && \
+                chmod -R 777 /usr/local/netdot/etc/ && \
+                date >> /srv/reloadconfig.log \
+            )" \
+        --on-modify-path-command "( \
+                /usr/local/netdot/export/ \
+            )=( \
+                chmod -R 777 /usr/local/netdot/export/ \
+            )"
+done
